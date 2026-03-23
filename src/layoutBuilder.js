8559e02f6530a24e42961d9796edc920a344c628
Stuart Wilkes <s.wilkes@wellcome.org>
2026-03-23 10:04:18 +0000

    Initial restructure of pdfmake accessibility

--- src/layoutBuilder.js
+++ src/layoutBuilder.js
@@ -20,6 +20,7 @@ var isFunction = require('./helpers').isFunction;
 var TextTools = require('./textTools');
 var StyleContextStack = require('./styleContextStack');
 var isNumber = require('./helpers').isNumber;
+var accessibilityMetadata = require('./accessibilityMetadata');
 
 function addAll(target, otherArray) {
 	otherArray.forEach(function (item) {
@@ -42,6 +43,10 @@ function LayoutBuilder(pageSize, pageMargins, imageMeasure, svgMeasure) {
 	this.svgMeasure = svgMeasure;
 	this.tableLayouts = {};
 	this.nestedLevel = 0;
+
+	// Accessibility context tracking
+	this._accessibilityListStack = [];
+	this._accessibilityTableContext = null;
 }
 
 LayoutBuilder.prototype.registerTableLayouts = function (tableLayouts) {
@@ -881,18 +886,34 @@ LayoutBuilder.prototype.processList = function (orderedList, node) {
 		items = orderedList ? node.ol : node.ul,
 		gapSize = node._gapSize;
 
+	// Push list accessibility context
+	this._accessibilityListStack.push({
+		node: node,
+		itemIndex: -1,
+		isFirstNodeInItem: false,
+		isLastNodeInItem: false
+	});
+
 	this.writer.context().addMargin(gapSize.width);
 
 	var nextMarker;
 	this.tracker.auto('lineAdded', addMarkerToFirstLeaf, function () {
-		items.forEach(function (item) {
+		items.forEach(function (item, idx) {
+			var listCtx = self._accessibilityListStack[self._accessibilityListStack.length - 1];
+			listCtx.itemIndex = idx;
+			listCtx.isFirstNodeInItem = true;
+			listCtx.isLastNodeInItem = (!item.ol && !item.ul);
 			nextMarker = item.listMarker;
 			self.processNode(item);
+			listCtx.isFirstNodeInItem = false;
 			addAll(node.positions, item.positions);
 		});
 	});
 
 	this.writer.context().addMargin(-gapSize.width);
+
+	// Pop list accessibility context
+	this._accessibilityListStack.pop();
 
 	function addMarkerToFirstLeaf(line) {
 		// I'm not very happy with the way list processing is implemented
@@ -922,6 +943,13 @@ LayoutBuilder.prototype.processTable = function (tableNode) {
 	this.nestedLevel++;
 	var processor = new TableProcessor(tableNode);
 
+	var isTaggedTable = accessibilityMetadata.shouldTagTable(tableNode);
+	var isTOCTable = accessibilityMetadata.isTOC(tableNode);
+	var headerRowCount = tableNode.table.headerRows || 0;
+
+	// Store the accessibility table context so child nodes (processLeaf) can see it
+	var previousTableContext = this._accessibilityTableContext;
+
 	processor.beginTable(this.writer);
 
 	var rowHeights = tableNode.table.heights;
@@ -937,6 +965,22 @@ LayoutBuilder.prototype.processTable = function (tableNode) {
 		}
 
 		processor.beginRow(i, this.writer);
+
+		// Set table accessibility context for this row's cells
+		if (isTaggedTable) {
+			var isHeaderRow = i < headerRowCount;
+			this._accessibilityTableContext = {
+				tagged: true,
+				isTOC: isTOCTable,
+				rowIndex: i,
+				isHeader: isHeaderRow,
+				headerRowCount: headerRowCount,
+				totalRows: l,
+				isFirstRow: i === 0,
+				isLastRow: i === l - 1,
+				colIndex: -1 // will be updated per cell during processRow
+			};
+		}
 
 		var height;
 		if (isFunction(rowHeights)) {
@@ -980,6 +1024,10 @@ LayoutBuilder.prototype.processTable = function (tableNode) {
 	}
 
 	processor.endTable(this.writer);
+
+	// Restore previous table context (for nested tables)
+	this._accessibilityTableContext = previousTableContext;
+
 	this.nestedLevel--;
 	if (this.nestedLevel === 0) {
 		this.writer.context().resetMarginXTopParent();
@@ -1022,7 +1070,26 @@ LayoutBuilder.prototype.processLeaf = function (node) {
 		}
 	}
 
+	// Build accessibility context for text lines
+	var accessibilityRole = accessibilityMetadata.getAccessibilityRole(node);
+	var isFirstLine = true;
+
 	while (line && (maxHeight === -1 || currentHeight < maxHeight)) {
+		// Annotate line with accessibility context
+		line._accessibilityContext = {
+			role: accessibilityRole,
+			isFirstLine: isFirstLine,
+			isLastLine: line.lastLineInParagraph,
+			listContext: this._accessibilityListStack.length > 0 ? {
+				depth: this._accessibilityListStack.length,
+				itemIndex: this._accessibilityListStack[this._accessibilityListStack.length - 1].itemIndex,
+				isFirstInItem: isFirstLine && this._accessibilityListStack[this._accessibilityListStack.length - 1].isFirstNodeInItem,
+				isLastInItem: line.lastLineInParagraph && this._accessibilityListStack[this._accessibilityListStack.length - 1].isLastNodeInItem
+			} : null,
+			tableContext: this._accessibilityTableContext
+		};
+		isFirstLine = false;
+
 		var positions = this.writer.addLine(line);
 		node.positions.push(positions);
 		line = this.buildNextLine(node);
@@ -1116,11 +1183,21 @@ LayoutBuilder.prototype.buildNextLine = function (textNode) {
 
 // images
 LayoutBuilder.prototype.processImage = function (node) {
+	node._accessibilityContext = {
+		role: (node.alt || node.actualText) ? 'Figure' : 'Artifact',
+		alt: node.alt,
+		actualText: node.actualText
+	};
 	var position = this.writer.addImage(node);
 	node.positions.push(position);
 };
 
 LayoutBuilder.prototype.processSVG = function (node) {
+	node._accessibilityContext = {
+		role: (node.alt || node.actualText) ? 'Figure' : 'Artifact',
+		alt: node.alt,
+		actualText: node.actualText
+	};
 	var position = this.writer.addSVG(node);
 	node.positions.push(position);
 };
@@ -1138,6 +1215,7 @@ LayoutBuilder.prototype.processCanvas = function (node) {
 	this.writer.alignCanvas(node);
 
 	node.canvas.forEach(function (vector) {
+		vector._accessibilityContext = { role: 'Artifact' };
 		var position = this.writer.addVector(vector);
 		node.positions.push(position);
 	}, this);
